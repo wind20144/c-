a493ef4c034b50e4c937cc07a0398028dcf5d269
wind20144 <107836758+wind20144@users.noreply.github.com>
2022-06-26 11:57:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите Число");
+  int number1 = Convert.ToInt32(Console.ReadLine());
+  int number2 = Convert.ToInt32(Console.ReadLine());
+            if (number1 > number2)
+                Console.WriteLine(number1);
+            else 
+                Console.WriteLine(number2);
+Console.WriteLine("Максимальное число ");
+
+Console.WriteLine("Введите Число");
+   int number1 = Convert.ToInt32(Console.ReadLine());
+   int number2 = Convert.ToInt32(Console.ReadLine());
+   int number3 = Convert.ToInt32(Console.ReadLine());
+         if (number1 > number2 && number1 > number3)
+                    Console.WriteLine("Максимальное число " + number1); 
+                else if (number2 > number3)
+                    Console.WriteLine("Максимальное число " + number2); 
+                else
+                    Console.WriteLine("Максимальное число " + number3);
+
+
+
+Console.WriteLine("Введите Число");
+   int number = Convert.ToInt32(Console.ReadLine());
+      if (number % 2 == 0) 
+      Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
+
+
+
+
+
+                
+
+ 
